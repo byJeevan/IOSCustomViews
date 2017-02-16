d3d082dfe0ba81e8176aa3ecc88330d26650e41f
Jeevan <jeevan8082@gmail.com>
2017-02-16 11:58:53 +0530

    Project workspace initial commit

---
+++ IOSCustomViews/IOSCustomViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSCustomViews
+//
+//  Created by Jeevan on 16/02/17.
+//  Copyright © 2017 com.byjeevan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOSCustomViews/IOSCustomViews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IOSCustomViews
+//
+//  Created by Jeevan on 16/02/17.
+//  Copyright © 2017 com.byjeevan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
